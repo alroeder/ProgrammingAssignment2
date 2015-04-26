29898bfa5fa57316ce4c7dff17cddebfaf5adf3b
alroeder <a.l.roeder@gmx.net>
2015-04-27 00:23:11 +0200

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -4,10 +4,10 @@
 makeCacheMatrix <- function(x = matrix()) {
   im <- NULL #variable to store inverse (initialised with NULL)
   set <- funtcion(y){
-    x <<- y # saves matrix into superior environment
+    x <<- y #saves matrix into superior environment
     im <<- NULL
   }
-  get <- function() x # get original matrix
+  get <- function() x #get original matrix
   setinverse <- function(inverse) im <<- inverse #set inverse
   getinverse <- function() im  #get/return inverse
   list(set = set, get = get,  #list of values of the makeCacheMatrix
@@ -16,17 +16,17 @@ makeCacheMatrix <- function(x = matrix()) {
   }
 
 
-# The cacheSolve function checks if the inverse of a special matrix has already been calculated in makeCacheMatrix
-# if yes, it retrieves the (already computed) inverse of the matrix from cache, otherwise it
-# computes the inverse, saves it to cache and returns it
+#The cacheSolve function checks if the inverse of a special matrix has already been calculated in makeCacheMatrix
+#if yes, it retrieves the (already computed) inverse of the matrix from cache, otherwise it
+#computes the inverse, saves it to cache and returns it
 cacheSolve <- function(x, ...) {
   im <- x$getinverse() #gets "value" of inverse from makeCacheMatrix
-  if(!is.null(im)) {  # if "value" of inverse is not NULL...message and
+  if(!is.null(im)) {  #if "value" of inverse is not NULL...message and
     message("getting cached data")
-    return(im)  # returns cached inverse via makeCacheMatrix
+    return(im)  #returns cached inverse via makeCacheMatrix
   }
   data <- x$get() #getting the original matrix because im = NULL (no inverse in cache) 
   im <- solve(data, ...) #computing the inverse of the matrix
   x$setinverse(im) #saves inverse to cache
-  im  	# prints inverse
+  im  	#prints inverse
 }
